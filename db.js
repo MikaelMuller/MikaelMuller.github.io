96cda94c6b82b3dd3502117393c661a350be2859
MikaelMuller <38796070+MikaelMuller@users.noreply.github.com>
2026-02-05 16:47:20 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -45,8 +45,10 @@ function enviarRegistro(eventId) {
     document.getElementById("enderecoEmail").value = "";
     document.getElementById("alergias").value = "";
     document.getElementById("emergencia").value = "";
+    document.getElementById("riscos").checked = false;
+    document.getElementById("lgpd").checked = false;
     
-    document.getElementById("avisoTexto").style = "color: black";
+    document.getElementById("avisoTexto").style = "color: white";
     document.getElementById("avisoTexto").innerHTML = "Inscrição realizada com sucesso!";
 
     vagasSalvas[eventId] = vagasSalvas[eventId] - 1;
@@ -67,19 +69,24 @@ DB.ref("eventos").once("value").then(function(snapshot) {
     var cor = data.cor;
 
     if(data.formulario){
-      if(Date.parse(data.dia) > new Date()){
+      if (Date.parse(data.dia) > Date.now() + 54000000) {
+        
         if(data.participantes != undefined){
           var vagasLivres = data.limite - Object.keys(data.participantes).length;
         }else{
           var vagasLivres = data.limite;
         }
         
-        meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "'+data.cor+'", "extendedProps": {"texto": "'+ data.texto + '", "vagas": "'+vagasLivres+'", "passou": false, "formulario": true}},'
+        if(vagasLivres > 0){
+          meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "'+data.cor+'", "extendedProps": {"texto": '+ JSON.stringify(data.texto) + ', "vagas": "'+vagasLivres+'", "passou": false, "formulario": true}},';
+        } else{
+          meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "gray", "extendedProps": {"texto": '+ JSON.stringify(data.texto) + ', "vagas": "'+vagasLivres+'", "passou": false, "formulario": true}},'
+        }
       } else{
-        meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "gray","extendedProps": {"texto": "'+ data.texto + '", "vagas": "0", "passou": true, "formulario": true}},'
+        meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "gray","extendedProps": {"texto": '+ JSON.stringify(data.texto) + ', "vagas": "0", "passou": true, "formulario": true}},';
       }
     } else{
-      meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "'+data.cor+'","extendedProps": {"texto": "'+ data.texto + '", "vagas": "0", "passou": false, "formulario": false}},'
+      meusEventos = meusEventos + '{"start": "' + data.dia + '", "title": "'+ data.nome + '", "id": "' + data.id + '", "backgroundColor": "'+data.cor+'","extendedProps": {"texto": '+ JSON.stringify(data.texto) + ', "vagas": "0", "passou": false, "formulario": false}},';
     }
   });
   meusEventos = meusEventos.slice(0, -1);
@@ -127,11 +134,11 @@ function gerarFormulario(eventId, texto, vagas, passou, formulario){
         vagasSalvas[eventId] = vagas;
       }
 
-      document.getElementById("eventoTexto").innerHTML = texto;
+      document.getElementById("eventoTexto").innerHTML = texto.replace(/\n/g, "<br>");
 
       if(vagasSalvas[eventId] > 0){
         document.getElementById("vagasTexto").innerHTML = '<p id="vagas">Vagas disponíveis: ' + vagasSalvas[eventId] + '</p>';
-        document.getElementById("eventoDiv").innerHTML = '<h1 id="formularioTexto">Formulário de Inscrição:</h1><p id="nomeTexto">Nome: *</p><input type="text" id="nome" placeholder="Seu nome..." /><p id="idadeTexto">Idade: *</p><input type="number" id="idade" placeholder="Sua idade..." /><p id="idadeTexto">Telefone para contato: *</p><input type="number" id="telefone" placeholder="Número de telefone..." /><p id="emailTexto">E-mail para contato: *</p><input type="email" id="enderecoEmail" placeholder="Seu email..." /><p id="emergenciaTexto">Nome e telefone para caso de emergências:</p><textarea id="emergencia" cols="30" rows="5" placeholder="Contato de emergência..."></textarea><p id="alergiasTexto">Possui alergias, necessidades especiais ou restrições?</p><textarea id="alergias" cols="30" rows="5" placeholder="Alergias, necessidades especiais ou restrições..."></textarea><br><br><input type="checkbox" id="riscos" name="riscos" value="riscos"><label for="riscos" id="riscosTexto">* DECLARO que conheço e assumo os riscos inerentes à atividade de visitação em áreas naturais abertas no interior do Parque Natural Municipal Grota Funda (PNMGF) e que me responsabilizo pelos meus acompanhantes nos passeios e trilhas, isentando a PNMGF e a Prefeitura da estância de Atibaia de qualquer responsabilidade em caso de problemas de saúde, mal súbito ou acidentes pessoais eventualmente ocorridos comigo ou com meus acompanhantes. DECLARO ESTAR CIENTE DE QUE: As áreas naturais do PNMGF apresentam riscos tais como: relevo acidentado, quedas, picadas de insetos e carrapatos, animais peçonhentos, rajadas de vento, quedas de árvores e rochas, deslizamentos de terra, raios, desmoronamentos, entre outros, sendo o visitante o maior responsável pela própria segurança. DECLARO AINDA ESTAR CIENTE DE QUE: Poderei ser responsabilizado por quaisquer danos causados por mim, ou pelos visitantes que estão sob meu acompanhamento, nos patrimônios (ambientais ou históricos) existentes no PNMGF.</label><br><input type="checkbox" id="lgpd" name="lgpd" value="lgpd"><label for="lgpd" id="lgpdTexto">* Aviso de Privacidade (LGPD): As informações coletadas neste formulário serão utilizadas exclusivamente para fins de inscrição, organização e comunicação das atividades do Parque Natural Municipal da Grota Funda. Os dados não serão compartilhados com terceiros e serão armazenados de forma segura, conforme a Lei Geral de Proteção de Dados (Lei nº 13.709/2018). Você autoriza o uso dos seus dados pessoais conforme descrito acima e de acordo com a LGPD?</label><br><br><button id="botao" onclick="enviarRegistro('+"'"+eventId+"'"+')">Enviar</button><br>';
+        document.getElementById("eventoDiv").innerHTML = '<h1 id="formularioTexto">Formulário de Inscrição:</h1><p id="nomeTexto">Nome: *</p><input type="text" id="nome" placeholder="Seu nome..." /><p id="idadeTexto">Idade: *</p><input type="number" id="idade" placeholder="Sua idade..." /><p id="idadeTexto">Telefone para contato: *</p><input type="text" id="telefone" placeholder="Número de telefone..." pattern="[0-9]*" inputmode="numeric"/><p id="emailTexto">E-mail para contato: *</p><input type="email" id="enderecoEmail" placeholder="Seu email..." /><p id="emergenciaTexto">Nome e telefone para caso de emergências:</p><textarea id="emergencia" cols="30" rows="5" placeholder="Contato de emergência..."></textarea><p id="alergiasTexto">Possui alergias, necessidades especiais ou restrições?</p><textarea id="alergias" cols="30" rows="5" placeholder="Alergias, necessidades especiais ou restrições..."></textarea><br><br><input type="checkbox" id="riscos" name="riscos" value="riscos"><label for="riscos" id="riscosTexto">* DECLARO que conheço e assumo os riscos inerentes à atividade de visitação em áreas naturais abertas no interior do Parque Natural Municipal Grota Funda (PNMGF) e que me responsabilizo pelos meus acompanhantes nos passeios e trilhas, isentando a PNMGF e a Prefeitura da estância de Atibaia de qualquer responsabilidade em caso de problemas de saúde, mal súbito ou acidentes pessoais eventualmente ocorridos comigo ou com meus acompanhantes. DECLARO ESTAR CIENTE DE QUE: As áreas naturais do PNMGF apresentam riscos tais como: relevo acidentado, quedas, picadas de insetos e carrapatos, animais peçonhentos, rajadas de vento, quedas de árvores e rochas, deslizamentos de terra, raios, desmoronamentos, entre outros, sendo o visitante o maior responsável pela própria segurança. DECLARO AINDA ESTAR CIENTE DE QUE: Poderei ser responsabilizado por quaisquer danos causados por mim, ou pelos visitantes que estão sob meu acompanhamento, nos patrimônios (ambientais ou históricos) existentes no PNMGF.</label><br><input type="checkbox" id="lgpd" name="lgpd" value="lgpd"><label for="lgpd" id="lgpdTexto">* Aviso de Privacidade (LGPD): As informações coletadas neste formulário serão utilizadas exclusivamente para fins de inscrição, organização e comunicação das atividades do Parque Natural Municipal da Grota Funda. Os dados não serão compartilhados com terceiros e serão armazenados de forma segura, conforme a Lei Geral de Proteção de Dados (Lei nº 13.709/2018). Você autoriza o uso dos seus dados pessoais conforme descrito acima e de acordo com a LGPD?</label><br><br><button id="botao" onclick="enviarRegistro('+"'"+eventId+"'"+')">Enviar</button><br>';
       } else{
         vagasEsgotadas();
       }
@@ -141,7 +148,7 @@ function gerarFormulario(eventId, texto, vagas, passou, formulario){
       document.getElementById("eventoDiv").innerHTML = "";
     }
   } else{
-    document.getElementById("eventoTexto").innerHTML = texto;
+    document.getElementById("eventoTexto").innerHTML = texto.replace(/\n/g, "<br>");
     document.getElementById("vagasTexto").innerHTML = "Não há formulário de inscrição para essa atividade.";
     document.getElementById("eventoDiv").innerHTML = "";
   }
